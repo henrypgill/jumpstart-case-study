91421730254481cc4a65e2c1d8f378a82ab56823
Henry Gill <hpgill312@gmail.com>
2024-12-16 16:49:47 +0000

    updated RTK to work with SSR with Next.JS

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, useStore } from "react-redux";
+import applicationsReducer from "./applicationsSlice";
 import candidatesReducer from "./candidatesSlice";
 import rolesReducer from "./rolesSlice";
-import applicationsReducer from "./applicationsSlice";
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
-export const store = configureStore({
+export type AppStore = ReturnType<typeof makeStore>
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+
+
+export const makeStore = () => {
+  return configureStore({
     reducer: {
         candidates: candidatesReducer,
         roles: rolesReducer,
         applications: applicationsReducer,
     },
-});
+})
+}
+
 
 //export the store's dispatch and selector functions for ease of use
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+// export const useAppDispatch: () => AppDispatch = useDispatch;
+// export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
+export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppStore = useStore.withTypes<AppStore>()
